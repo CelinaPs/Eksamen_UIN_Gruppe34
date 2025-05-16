f32f44f0a0462a8305fa2088227ab776e001fa3c
CelinaPs <143390437+CelinaPs@users.noreply.github.com>
2025-05-16 19:24:03 +0200

    Pusher App, Cat og starten på Home + kommentarer

---
+++ Eksamen/src/components/Home.jsx
@@ -0,0 +1,16 @@
+import "../Home.css";
+
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+export default function Home() {
+  const API_KEY = "5OYpmz3EGOfKvtU8vlLA3cC6ASbAeGgE"; //api nøkkel til ticketmaster
+  const FESTIVAL_IDS = [ //liste med festival-id som vi ønsker data fra
+    "K8vZ917_YJf", //NEON
+    "K8vZ917oWOV", //Tons of Rock
+    "K8vZ917K7fV", //Findings
+    "K8vZ917bJC7", //Skeikampenfestivalen
+  ];
+
+  //ChatGPT under her
+}
